134bd0794056a73e3d110f132f315412d278a76f
Jacob Cunningham <jcun4128@gmail.com>
2022-10-31 20:12:17 -0500

    quitter

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Dexie from 'dexie';
+import './css-reset.css';
 import './App.css';
 
 const App = () => {
   const [offlineStorage, setOfflineStorage] = useState(null);
+  const [itemName, setItemName] = useState('');
+  const [items, setItems] = useState([]);
 
   // id, name
   const setupOfflineStorage = () => {
     const db = new Dexie("ShoppingList");
 
     db.version(1).stores({
-      items: "++id,name",
+      items: "++id,name,checked",
     });
 
     setOfflineStorage(db);
   };
 
-  useEffect(() => {
+  const saveItem = () => {
     if (!offlineStorage) return;
 
     offlineStorage.transaction('rw', offlineStorage.items, async() => {
       if (
         await offlineStorage.items.add({
-          name: 'test'
+          name: itemName,
+          checked: 'false'
         }).then((insertedId) => {
           console.log('inserted');
           return true;
         })
       ) {
         console.log('inserted');
+        setItemName('');
+        renderItems();
       } else {
         alert('Failed to save item');
       }
@@ -37,7 +43,28 @@ const App = () => {
       console.log('failed to save photos', e);
       alert('Failed to save photos to device');
     });
-  }, [offlineStorage]);
+  }
+
+  const renderItems = () => {
+    offlineStorage.items.toArray().then((items) => {
+      setItems(items);
+    });
+  }
+
+  const resetDb = () => {
+    offlineStorage.delete().then(() => {
+      setOfflineStorage(null);
+    })
+    .catch((err) => {
+      alert('failed to reset');
+    })
+  }
+
+  useEffect(() => {
+    if (items) {
+      console.log(items);
+    }
+  }, [items]);
  
   useEffect(() => {
     if (!offlineStorage) setupOfflineStorage();
@@ -45,7 +72,14 @@ const App = () => {
 
   return (
     <div className="App">
-      
+      <div className="App__header">
+        <button type="button" onClick={(e) => saveItem()}>+</button>
+        <input type="text" placeholder="item name" value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+      </div>
+      <div className="App__body">
+
+      </div>
+      <button className="reset-btn" type="button" onClick={(e) => resetDb()}>Reset</button>
     </div>
   );
 }
